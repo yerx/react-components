f4f1e10756d8195d6ca9bc7d1ad7f8a396dd52a4
yerx <yer.x.xiong@gmail.com>
2019-12-30 16:22:00 -0500

    update

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // TODO
+
+var GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>{this.props.groceryItem}</li>
+    );
+  }
+}
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(groceryItem =>
+      <GroceryListItem groceryItem={groceryItem} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h1>Grocery List</h1>
+    <GroceryList groceryItems={['Bananas', 'Chocolate']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
